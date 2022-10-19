0c87134cf0af712c091f0e3e7b063f3f1f172fbe
DutchenkoNikita <dutchenkonekkit@gmail.com>
2022-10-19 17:11:22 +0300

    Update sum_in_array.py

--- sum_in_array.py
+++ sum_in_array.py
@@ -8,3 +8,27 @@ for Sierentz Global Merchants
 
 Test task
 """
+import numpy as np
+
+
+def find_sum(arr, res):
+    arr.sort()
+    final_arr = np.array([])
+    start, end = 0, arr.size - 1
+    while start < end:
+        temp_res = arr[start] + arr[end]
+        if temp_res == res:
+            final_arr = np.append(final_arr, [arr[start], arr[end]])
+            start += 1
+            end -= 1
+        elif temp_res < res:
+            start += 1
+        else:
+            end -= 1
+    if final_arr.size == 0:
+        return -1
+    return final_arr
+
+arr_1 = np.array([-3, 1, 4, 6])
+
+print(find_sum(arr_1, 7))
